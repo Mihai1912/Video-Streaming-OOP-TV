181d53f6160c742c88be8bf67eee0730f707759f
Mihai1912 <mihai.popescu1109@gmail.com>
2023-01-03 19:24:28 +0200

    Adding functionality to expand the movie database

--- src/pages/type/Database.java
+++ src/pages/type/Database.java
@@ -26,6 +26,29 @@ public class Database {
     public void database(Input input, Action action, Helper helper) {
         if (action.getFeature().equals("add")) {
 
+            for (Movie movie : input.getMovies()) {
+                if (action.getAddedMovie().getName().equals(movie.getName())) {
+                    helper.printError();
+                    return;
+                }
+            }
+
+            input.getMovies().add(action.getAddedMovie());
+
+            for (User user : helper.getUsers()) {
+                if (!action.getAddedMovie().getCountriesBanned().contains(user.getCredentials().getCountry())) {
+                    user.getMoviesToWatch().add(action.getAddedMovie());
+                    for (String genre : action.getAddedMovie().getGenres()) {
+                        if (user.getSubscriptions().contains(genre)) {
+                            Notification notification = new Notification();
+                            notification.setMovieName(action.getAddedMovie().getName());
+                            notification.setMessage("ADD");
+                            user.getNotifications().add(notification);
+                        }
+                    }
+                }
+            }
+
         } else {
 
         }
